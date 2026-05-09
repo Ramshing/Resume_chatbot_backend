870e81803c9ffdab100f49d206aa6bd638f75805
root <root@LAPTOP-67FB9HNK.>
2026-05-09 18:35:03 +0000

    updated the requirements.txt

--- src/database/db_connections.py
+++ src/database/db_connections.py
@@ -1,12 +1,35 @@
+# from psycopg2.pool import SimpleConnectionPool
+
+# pool = SimpleConnectionPool(
+#     1, 10,
+#     dbname="Resume-Shortlist-RAG",
+#     user="postgres",
+#     password="1234",
+#     host="192.168.1.2",
+#     port="5432"
+# )
+
+# def get_connection():
+#     return pool.getconn()
+
+# def release_connection(conn):
+#     pool.putconn(conn)
+
+#````````````````````Render DB Connection``````````````````````````````
+
+import os
 from psycopg2.pool import SimpleConnectionPool
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 pool = SimpleConnectionPool(
-    1, 10,
-    dbname="Resume-Shortlist-RAG",
-    user="postgres",
-    password="1234",
-    host="192.168.1.3",
-    port="5432"
+    minconn=1,
+    maxconn=10,
+    dsn=DATABASE_URL,
+    sslmode="require"
 )
 
 def get_connection():
